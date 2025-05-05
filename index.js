8d0d50f992ad26974f4b82ea3b617ba2ea497ef9
Md-Salman-Rahman339 <salmanrahman339@gmail.com>
2025-05-05 20:17:48 +0600

    Save menu item to the server and make api secure

--- index.js
+++ index.js
@@ -143,6 +143,11 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
           const result = await menuCollection.find().toArray();
           res.send(result);
       })
+      app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
+        const item = req.body;
+        const result = await menuCollection.insertOne(item);
+        res.send(result);
+      })
       
       app.get('/reviews', async(req, res) =>{
           const result = await reviewCollection.find().toArray();
